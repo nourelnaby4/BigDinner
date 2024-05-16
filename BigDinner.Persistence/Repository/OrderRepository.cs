711333652088da7186ce0e9048c9bb2eb35a2631
nourelnaby4 <nourelnaby4@gmail.com>
2024-05-17 02:50:25 +0300

    refact: enhance code in get order cache

--- BigDinner.Persistence/Repository/OrderRepository.cs
+++ BigDinner.Persistence/Repository/OrderRepository.cs
@@ -19,23 +19,6 @@ public class OrderRepository : IOrderRepository
         => _context.Add(order);
 
 
-    //public async Task<IEnumerable<Order>> GetAll()
-    //{
-    //    var key = $"orderList";
-
-    //    if (!_memoryCacheService.TryGetValue(key, out List<Order> orders))
-    //    {
-    //        orders = await _context.Orders
-    //        .Include(x => x.Items)
-    //        .Include(x => x.Shipping)
-    //        .ToListAsync(); ;
-
-    //        _memoryCacheService.Set(key, orders);
-    //    }
-
-    //    return orders;
-    //}
-
     public async Task<IEnumerable<Order>> GetAll()
     {
         var key = "orderList";
@@ -52,12 +35,11 @@ public class OrderRepository : IOrderRepository
     {
         var key = $"order={orderId}";
 
-        if (!_memoryCacheService.TryGetValue(key, out Order order))
+        return await _memoryCacheService.Get(key, async () =>
         {
-            order = await _context.Orders
+           return await _context.Orders
            .Include(x => x.Items)
            .FirstOrDefaultAsync(x => x.Id == orderId);
-        }
-        return order;
+        });
     }
 }
